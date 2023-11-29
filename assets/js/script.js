dc6362428e37f2db2f63026962b9202623d4d5ed
Andy Pierce <spudos16@gmail.com>
2023-11-29 14:32:29 +0000

    addition game working

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,29 +3,57 @@
 
 document.addEventListener("DOMContentLoaded", function() {
   let buttons = document.getElementsByTagName("button");
-
+  
   for (let button of buttons) {
       button.addEventListener("click", function() {
           if (this.getAttribute("data-type") === "submit") {
-              alert("You clicked Submit!");
+              checkAnswer();
           } else {
               let gameType = this.getAttribute("data-type");
-              alert(`You clicked ${gameType}`);
+              runGame(gameType);
           }
       });
   }
+
+  runGame("addition");
+
 });
 
-function runGame() {
+function runGame(gameType) {
+  let num1 = Math.floor(Math.random() * 25) + 1;
+  let num2 = Math.floor(Math.random() * 25) + 1;
 
+  if (gameType === "addition") {
+    displayAdditionQuestion(num1, num2);
+  } else {
+    alert(`Unknown game type: ${gameType}`);
+    throw `Unknown game type: ${gameType}. Aborting!`;
+  }
 }
 
 function checkAnswer() {
-
+  let userAnswer = parseInt(document.getElementById("answer-box").value);
+  let calculatedAnswer = calculateCorrectAnswer();
+  let isCorrect = userAnswer === calculatedAnswer[0];
+  if (isCorrect) {
+    alert(`Hey! You got it right! :D`);
+  } else {
+    alert(`Awww... you answered ${userAnswer}.  The correct answer was: ${calculatedAnswer[0]}`);
+  }
+  runGame(calculatedAnswer[1]);
 }
 
-function calculateCorrectanswer() {
+function calculateCorrectAnswer() {
+  let operand1 = parseInt(document.getElementById("operand1").innerText);
+  let operand2 = parseInt(document.getElementById("operand2").innerText);
+  let operator = document.getElementById("operator").innerText;
 
+  if (operator === "+") {
+    return [operand1 + operand2, "addition"];
+  } else {
+    alert(`Unimplimented operator: ${operator}`);
+    throw `Unimplimented operator: ${operator}. Aborting!`;
+  }
 }
 
 function incrementScore() {
@@ -36,8 +64,10 @@ function incrementWrongAnswer() {
   
 }
 
-function displayAdditionQuestion() {
-  
+function displayAdditionQuestion(operand1, operand2) {
+  document.getElementById('operand1').textContent = operand1;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
