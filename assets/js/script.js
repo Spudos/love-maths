6484e1458f0c75ff25340b4dcdfeeebe4fbb2434
Andy Pierce <spudos16@gmail.com>
2023-11-29 14:57:47 +0000

    multiply game working

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,6 +25,8 @@ function runGame(gameType) {
 
   if (gameType === "addition") {
     displayAdditionQuestion(num1, num2);
+  } else if (gameType === "multiply") {
+    displayMultiplyQuestion(num1, num2);
   } else {
     alert(`Unknown game type: ${gameType}`);
     throw `Unknown game type: ${gameType}. Aborting!`;
@@ -52,7 +54,8 @@ function calculateCorrectAnswer() {
 
   if (operator === "+") {
     return [operand1 + operand2, "addition"];
-    
+  } else if (operator === "x") {
+    return [operand1 * operand2, "multiply"];
   } else {
     alert(`Unimplimented operator: ${operator}`);
     throw `Unimplimented operator: ${operator}. Aborting!`;
@@ -62,7 +65,7 @@ function calculateCorrectAnswer() {
 
 function incrementScore() {
   let oldScore = parseInt(document.getElementById("score").innerText);
-    document.getElementById("score").innerText = ++oldScore;
+  document.getElementById("score").innerText = ++oldScore;
 }
 
 function incrementWrongAnswer() {
@@ -80,8 +83,11 @@ function displaySubtractQuestion() {
   
 }
 
-function displayMultiplyQuestion() {
-  
+function displayMultiplyQuestion(operand1, operand2) {
+  document.getElementById('operand1').textContent = operand1;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = "x";
+
 }
 
 function displayDivideQuestion() {
