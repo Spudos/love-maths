8a7cbf65b3b561bb87793e41c301f21b29faf034
Andy Pierce <spudos16@gmail.com>
2023-11-29 13:03:29 +0000

    create basic js structure

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectanswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+  
+}
+
+function displayAdditionQuestion() {
+  
+}
+
+function displaySubtractQuestion() {
+  
+}
+
+function displayMultiplyQuestion() {
+  
+}
+
+function displayDivideQuestion() {
+  
+}
